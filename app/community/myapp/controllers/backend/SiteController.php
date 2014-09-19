162addc900dd64b521ba92f8f7fe622a9726fb35
ethan <dongming.cui@jago-ag.cn>
2014-09-19 16:00:25 +0800

    add module model,service,controller

---
+++ app/community/myapp/controllers/backend/SiteController.php
@@ -0,0 +1,35 @@
+<?php
+namespace app\community\myapp\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+
+/**
+ * Site controller
+ */
+class SiteController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+
+}
