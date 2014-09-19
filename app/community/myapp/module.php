aa5a457dcce6a507e6feb350679a2371ee3e1765
ethan <dongming.cui@jago-ag.cn>
2014-09-19 13:37:56 +0800

    modules path define

---
+++ app/community/myapp/module.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\community\myapp;
+
+
+class module extends \yii\base\Module
+{
+	public $controllerNamespace = 'app\community\myapp\controllers';
+
+	public function init()
+	{
+		parent::init();
+
+		// custom initialization code goes here
+	}
+}
