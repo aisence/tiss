aabd59b40d6e362f98d16530db36f0a77b97eec6
ethan <dongming.cui@jago-ag.cn>
2014-09-19 15:46:50 +0800

    add module sql and config

---
+++ app/core/install/module.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\core\install;
+
+
+class module extends \yii\base\Module
+{
+	public $controllerNamespace = 'app\core\install\controllers';
+
+	public function init()
+	{
+		parent::init();
+
+		// custom initialization code goes here
+	}
+}
---
+++ app/local/loan/config/config.php
@@ -0,0 +1,3 @@
+<?php
+
+return array_merge(require(__DIR__ . '/app/local/loan/config/main.php'));
